d5920c29bfb7798981e6944b810698e2873e5951
inverse9 <permanawahyu97@gmail.com>
2024-12-31 16:30:16 +0800

    barang update, get by id, delete

--- routes/barang.js
+++ routes/barang.js
@@ -18,4 +18,33 @@ router.get("/", async (req, res) => {
   res.status(200).json(data);
 });
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { data, error } = await supabase
+    .from("barang")
+    .select("*")
+    .eq("id", id)
+    .single();
+  if (error) return res.status(400).json({ error: error.message });
+  res.status(200).json(data);
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { laundry_id, user_id, berat, harga } = req.body;
+  const { data, error } = await supabase
+    .from("barang")
+    .update({ laundry_id, user_id, berat, harga })
+    .eq("id", id);
+  if (error) return res.status(400).json({ error: error.message });
+  res.status(200).json(data);
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { data, error } = await supabase.from("barang").delete().eq("id", id);
+  if (error) return res.status(400).json({ error: error.message });
+  res.status(200).json(data);
+});
+
 module.exports = router;
